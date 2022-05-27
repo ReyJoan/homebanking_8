61cbeb6de1dcd6453e5a6b6bee2803ae75f15cc6
AllegriM <elmonky99@gmail.com>
2022-05-27 13:26:05 -0300

    Login arreglado con LS

--- public/styles/main.js
+++ public/styles/main.js
@@ -11,15 +11,20 @@ const form1 = document.querySelector('.register-form');
 const form2 = document.querySelector('.logIn-form');
 const form3 = document.querySelector('.main-form');
 
+// FORMULARIO REGISTER
 const user = document.querySelector('#user');
 const password = document.querySelector('#clav');
 const botonFormR = document.querySelector('#btn-registro');
+// FORMULARIO OPCION
 const botonLog = document.querySelector('#Log');
 const botonReg = document.querySelector('#Reg');
+// FORMULARIO LOGIN
+const userLogin = document.querySelector("#user-name");
+const passLogin = document.querySelector("#user-pass");
 const botonEntry = document.querySelector('#entry');
 
 const storedToDos = localStorage.getItem('User');
-
+let parsedValues = JSON.parse(storedToDos)
 
 botonLog.addEventListener('click', ()=>{
     form3.style.display = 'none';
@@ -31,23 +36,18 @@ botonReg.addEventListener('click', ()=>{
     form1.style.display = "block";
 })
 
-function guardarDatos (){
-    if (user.value == "" || password.value == "") {
-        alert('Debes ingresar un valor');
-    }
-    else {
-        let valorUsuario = user.value;
-        let valorPassword = password.value;
+botonFormR.addEventListener('click',(e)=>{
+    let valorUsuario = user.value;
+    let valorPassword = user.value;
+    e.preventDefault();
+    if (valorUsuario == "" || valorPassword == "") {
+        alert('Ingrese un valor en el campo');
+    }else {
         userData = [valorUsuario, valorPassword];
         localStorage.setItem("User", JSON.stringify(userData));
         form1.style.display = 'none';
         form2.style.display = "block";
     }
-    return userData
-}
-
-botonFormR.addEventListener('click',(e)=>{
-    e.preventDefault();
     // Podríamos usar un while mas adelante
     // if (user.value == "" || password.value == "") {
     //     alert('Debes ingresar un valor');
@@ -61,33 +61,20 @@ botonFormR.addEventListener('click',(e)=>{
     //     form2.style.display = "block";
     // }
     // return keys
-    // window.location.href = 'file:///C:/Users/Lenovo/Desktop/ITBA/homebanking_8/views/home.html' Link para enlazar a otras paginas de forma local
-    guardarDatos()
 }) 
 
 botonEntry.addEventListener('click', (e)=> {
     e.preventDefault();
-    guardarDatos();
-    let valorUsuario = user.value;
-    let valorPassword = password.value;
-    if (valorUsuario == storedToDos[0] && valorPassword == storedToDos[1]) {
-      window.location.href = "home.html";  
+    let valor = localStorage.getItem("User");
+    let valorParse = JSON.parse(valor);
+    console.log(valorParse)
+    let valorUsuario = userLogin.value;
+    let valorPassword = passLogin.value;
+    if (valorUsuario == valorParse[0] && valorPassword == valorParse[1]) {
+        window.location.href = "home.html";  
+    }else{
+        alert('ERROR EN EL LOGIN, USUARIO INCORRECTO')
     }
 })
-
-
-console.log(JSON.parse(storedToDos)) 
-for (let i = 0; i < localStorage.length; i++) {
-    let key = localStorage.key(i);
-    let valor = localStorage.getItem(key);
-    console.log(key);
-    console.log(valor);
-}
-
-
-
-
-
-
 
 
